f1c6648138d6f601d09462a851d84d14ac78fa54
Wided BEN OUNIS <wided.benounis@teamwillgroup.com>
2023-11-06 14:34:03 +0100

    Composition Api In Type Script

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app">
+    <p>
+      <!-- interpolation -->
+      {{ name }} - {{ age }}
+      <button @click="changeName('hayfa')">change name</button>
+      <button @click="changeAge(70)">change age</button>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    // use ref , toRefs reactive : compostion Api
+    const name = ref("Link");
+    // use generic variables
+    const age = ref<String | number>(25);
+
+    const changeName = (newName: string) => {
+      name.value = newName;
+      return newName;
+    };
+
+    const changeAge = (newAge: number) => {
+      age.value = newAge;
+      return newAge;
+    };
+
+    return { name, age, changeName, changeAge };
+  },
+});
+</script>
+
+<style>
+</style>
